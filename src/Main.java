7616e226767a587d236f7df7c863d077101078c0
Alinailka <alina-il@mail.ru>
2022-02-07 19:21:54 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 300;
+        int depositAmount = 10000;
+        boolean depositAmount1000 = true;
+
+        int totalAmount = depositAmount1000 ? depositAmount / 100 + amount + depositAmount : depositAmount + amount;
+        System.out.println("Итоговый баланс:" + totalAmount);
+
+         int bonus = depositAmount1000 ? depositAmount / 100 : 0;
+        System.out.println("Вам начислены бонусы в размере:" + bonus);
+    }
+}
