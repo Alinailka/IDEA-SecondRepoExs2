fc3dd546d8f16228f073eb742792513c9a05fb7e
Alinailka <alina-il@mail.ru>
2022-02-13 22:14:21 +0700

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@ public class Main {
         int amount = 300;
         int depositAmount = 10000;
         boolean depositAmount1000 = true;
-
         int totalAmount = depositAmount1000 ? depositAmount / 100 + amount + depositAmount : depositAmount + amount;
         System.out.println("Итоговый баланс:" + totalAmount);
-
-         int bonus = depositAmount1000 ? depositAmount / 100 : 0;
+        int bonus = depositAmount1000 ? depositAmount / 100 : 0;
         System.out.println("Вам начислены бонусы в размере:" + bonus);
     }
 }
